0106d875c87aea29742a06aef009ed0cac2b0751
NguyenCD <dinhnguyen.k15@gmail.com>
2022-04-18 09:29:37 +0700

    [Fix]-Fix 'href'

--- src/components/Decorations.js
+++ src/components/Decorations.js
@@ -1,34 +1,42 @@
 import React from "react";
-import '../css/decorations.css';
-import pic1 from '../img/decoration1.png';
-import pic2 from '../img/decoration2.png';
-import pic3 from '../img/decoration3.png';
+import "../css/decorations.css";
+import pic1 from "../img/decoration1.png";
+import pic2 from "../img/decoration2.png";
+import pic3 from "../img/decoration3.png";
 
 const Decorations = () => {
-    return (
-        <section className="decoration section bd-container" id="decoration">
-                <h2 className="section-title">Give Christmas Decorations <br/> For Your Home</h2>
-                <div className="decoration__container bd-grid">
-                    <div className="decoration__data">
-                        <img src={pic1} alt="" className="decoration__img"/>
-                        <h3 className="decoration__title">Christmas Bells</h3>
-                        <a href="#" className="button button-link">Go Shopping</a>
-                    </div>
+  return (
+    <section className="decoration section bd-container" id="decoration">
+      <h2 className="section-title">
+        Give Christmas Decorations <br /> For Your Home
+      </h2>
+      <div className="decoration__container bd-grid">
+        <div className="decoration__data">
+          <img src={pic1} alt="" className="decoration__img" />
+          <h3 className="decoration__title">Christmas Bells</h3>
+          <a href="#section" className="button button-link">
+            Go Shopping
+          </a>
+        </div>
 
-                    <div className="decoration__data">
-                        <img src={pic2} alt="" className="decoration__img"/>
-                        <h3 className="decoration__title">Christmas Candies</h3>
-                        <a href="#" className="button button-link">Go Shopping</a>
-                    </div>
+        <div className="decoration__data">
+          <img src={pic2} alt="" className="decoration__img" />
+          <h3 className="decoration__title">Christmas Candies</h3>
+          <a href="#section" className="button button-link">
+            Go Shopping
+          </a>
+        </div>
 
-                    <div className="decoration__data">
-                        <img src={pic3} alt="" className="decoration__img"/>
-                        <h3 className="decoration__title">Christmas Trees</h3>
-                        <a href="#" className="button button-link">Go Shopping</a>
-                    </div>
-                </div>
-            </section>
-    );
-}
+        <div className="decoration__data">
+          <img src={pic3} alt="" className="decoration__img" />
+          <h3 className="decoration__title">Christmas Trees</h3>
+          <a href="#section" className="button button-link">
+            Go Shopping
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Decorations;
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,8 +19,13 @@ const Header = () => {
   return (
     <header className="l-header" id="header">
       <nav className="nav bd-container">
-        <a href="#" className="nav__logo">Christmas Gift</a>
-        <div className={`nav__menu ${toggleMenu ? "show-menu" : ""} `} id="nav-menu">
+        <a href="#section" className="nav__logo">
+          Christmas Gift
+        </a>
+        <div
+          className={`nav__menu ${toggleMenu ? "show-menu" : ""} `}
+          id="nav-menu"
+        >
           <ul className="nav__list">
             <li className="nav__item">
               <a href="#home" className="nav__link active-link">
